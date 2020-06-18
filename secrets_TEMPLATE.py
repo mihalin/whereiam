443133db5fff20b00c2510e7d11b06930ecfb1bd
mihalin <stopa3.10.1996@mail.ru>
2020-06-18 23:41:13 +0300

    Add secrets template file

---
+++ secrets_TEMPLATE.py
@@ -0,0 +1,5 @@
+# This is a template file. You must write ur own secrets.py file
+
+token = "<Telegram bot token must be here>"
+yandex = "<Yandex maps API key must be here>"
+goldname = "<Telegram root user nickname (user who have permissions to update coordinates)>"
